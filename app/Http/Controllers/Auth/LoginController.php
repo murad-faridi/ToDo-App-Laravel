bd45107deb488fb31ea928ab31bd532f4bcabbb4
Murad Faridi <muradfaridi92@gmail.com>
2019-03-29 16:02:40 +0500

    Update Register and Login Routes

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -25,7 +25,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/todo';
 
     /**
      * Create a new controller instance.
