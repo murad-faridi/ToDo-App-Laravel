2a06b5737b19cdb491d9f64e8e77d1fd70465b1d
Murad Faridi <muradfaridi92@gmail.com>
2019-03-29 16:01:12 +0500

    Create ToDo Routes

--- routes/web.php
+++ routes/web.php
@@ -12,9 +12,7 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('/login');
 });
-
 Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
+Route::resource('todo','TodoController');
