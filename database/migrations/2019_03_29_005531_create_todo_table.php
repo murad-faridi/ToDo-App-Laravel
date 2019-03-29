5758ca27af05db6a3c6158acf203a58b3cdb98b4
Murad Faridi <muradfaridi92@gmail.com>
2019-03-29 07:40:55 +0500

    Create tables for migration
    Create a new migration table for Todo and make changes in Users table

--- database/migrations/2019_03_29_005531_create_todo_table.php
+++ database/migrations/2019_03_29_005531_create_todo_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateTodoTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,15 +13,14 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('todo', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->string('userimage');
-            $table->string('api_key')->nullable()->unique();
-            $table->rememberToken();
+            $table->string('todo');
+            $table->string('description');
+            $table->string('category');
+            $table->integer('user_id')->unsigned();
             $table->timestamps();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
         });
     }
 
@@ -32,6 +31,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('todo');
     }
 }
